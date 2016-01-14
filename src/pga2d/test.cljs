2cb0c671fc75835bac6dd6ad6cf6762bbb0280d0
Charles Gunn <skydog23@git>
2016-01-14 21:39:30 +0100

    work on ns command

--- src/pga2d/test.cljs
+++ src/pga2d/test.cljs
@@ -2,19 +2,22 @@
   (:require [pga2d.grassmann :as gr]
             [pga2d.clifford :as cf]))
 
+
 (enable-console-print!)
 
-(defn doit [sig]
+(def g (cf/ga 0))
 
-  (def g (cf/ga sig))
+; generate some global points and lines for testing
+(def p0 (gr/point 0 0 1))
+(def p1 (gr/point 1 0 0))
+(def p2 (gr/point 0 1 0))
+(def v0 (gr/point 2 3 0))
 
-  (def p0 (gr/point 0 0 1))
-  (def p1 (gr/point 1 0 0))
+(def m0 (gr/line 0 0 1))
+(def p3 (gr/point 2 3 1))
+(def m1 (gr/line 1 0 0))
+(def m2 (gr/line 0 1 0))
+(def m3 (gr/line .6 .8 1))
 
-  (println "this is the test!")
+(def mv1 (gr/multivector 1 [0 0 0] [4 5 6] 7))
 
-  (println (str "the gp (signature " sig " of p0 and p1 is: " ((g :gp) p0 p1)))
-  
-  )
-
-(doit 0)
